e5409b77244625258e8bf391a7d0914b6158aea5
zhengxii921 <aqua.inm@gmail.com>
2022-04-03 23:35:10 +0900

    remove

--- myapps/mysite/numazutourist/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Review, Place, Weekday
-
-# Register your models here.
-
-admin.site.register(Review)
-admin.site.register(Place)
-admin.site.register(Weekday)
--- myapps/mysite/numazutourist/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NumazutouristConfig(AppConfig):
-    name = 'numazutourist'
